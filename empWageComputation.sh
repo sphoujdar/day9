336265f0d42b9a6e192a34798492d06bc4cf30e8
Shubham <sm.phoujdar@gmail.com>
2020-11-08 18:59:30 +0530

    Add : Implemented Case logic

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -2,36 +2,39 @@
 
 echo Welcome to the Employee Wage Management Program
 
-#33% Chance that employee is absent
-attendance=$((RANDOM%3))
-#25% Chance that employee is part time
-((employee_type=RANDOM%4))
 
-if [ $attendance -eq 1 ]
-then
-	echo Employee is Present
-	if [ $employee_type -eq 1 ]
-	then
-		echo Employee works Part Time
-	else
-		echo Employee works Full Time
-	fi
-else
-	echo Employee is Absent
-fi
-
+#Employee Related Config
 part_time_hour=6
 full_time_hour=8
 wage_per_hour=20
+#33% Chance that employee is absent
+((attendance=RANDOM%3))
+#25% Chance that employee is part time
+((employee_type=RANDOM%4))
 
-if [ $attendance -eq 1 ]
-then
-	if [ $employee_type -ne 1 ]
-	then
-		((employee_wage=$full_time_hour*$wage_per_hour))
-	else
-		((employee_wage=$part_time_hour*$wage_per_hour))
-	fi
+
+#Creating a concatenated string to store individual employee related config
+individual_employee_config=$attendance$employee_type
+
+
+case $individual_employee_config in
+	#if attendance is 0 , employee is absent
+	00|01|02|03)
+	echo Employee is Absent;;
+
+	#if employee_type is 0 , employee is part time
+	10)
+	echo Employee is Present
+	echo Employee is works Part Time
+	((employee_wage=$part_time_hour*$wage_per_hour))
 	echo Wage for $(date +'%d-%m-%y') is $employee_wage
-fi
+	;;
 
+	#all other cases employee is Present and working full time
+	*)
+	echo Employee is Present
+        echo Employee works Full Time
+        ((employee_wage=$full_time_hour*$wage_per_hour))
+        echo Wage for $(date +'%d-%m-%y') is Rs.$employee_wage
+	;;
+esac
