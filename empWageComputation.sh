530af5dc5c4f571264000bde554232e89f3ddc56
Shubham <sm.phoujdar@gmail.com>
2020-11-08 16:21:15 +0530

    Added code for Randomly generating Present/Absent Employee.

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,3 +1,11 @@
 #!/bin/bash -x
 
 echo Welcome to the Employee Wage Management Program
+
+attendance=$((RANDOM%2))
+
+if [ $attendance -eq 1]
+	echo Employee is Present
+else
+	echo Employee is absent
+fi
