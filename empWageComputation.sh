c84d609a21c91a2d08de2ab902cf3b1bca551dfc
Shubham <sm.phoujdar@gmail.com>
2020-11-08 17:06:17 +0530

    Add : Calculate Daily Wage

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -2,7 +2,8 @@
 
 echo Welcome to the Employee Wage Management Program
 
-attendance=$((RANDOM%2))
+#33% Chance that employee is absent
+attendance=$((RANDOM%3))
 
 if [ $attendance -eq 1 ]
 then
@@ -10,3 +11,13 @@ then
 else
 	echo Employee is Absent
 fi
+
+full_day_hour=8
+wage_per_hour=20
+
+if [ $attendance -eq 1 ]
+then
+	((employee_wage=$full_day_hour*$wage_per_hour))
+	echo Wage for $(date +'%d-%m-%y') is $employee_wage
+fi
+
