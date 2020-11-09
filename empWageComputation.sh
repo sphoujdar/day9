0deee04d8e0dda2fec0603b5113d1fb15000feb2
Shubham <sm.phoujdar@gmail.com>
2020-11-09 08:38:24 +0530

    Refactor : Function to get work hours

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -2,6 +2,28 @@
 
 echo Welcome to the Employee Wage Management Program
 
+#Function to get work hours
+
+get_work_hours () {
+
+type_of_employee=$1
+
+if [ $type_of_employee -eq 0 ]
+then
+	#Part time employee may work from 4 to 7 hours
+        ((part_time_hour=$((RANDOM%4))+4))
+	todays_hours=$part_time_hour
+else
+	#Full time employee may work from 6 to 9 hours
+	((full_time_hour=$((RANDOM%4))+6))
+	todays_hours=$full_time_hour
+fi
+
+return $todays_hours
+
+}
+
+
 
 #Employee Related Config
 wage_per_hour=20
@@ -11,14 +33,34 @@ days_attended=0
 total_hours=0
 
 #Calculate Days attended and hours worked
-for working_day in {1..20}
+for working_day in {1..21}
 do
+
+	#Calculate wage for a maximum of 20 days or 100 hours
+        if [ $days_attended -eq 21 -o $total_hours -ge 100 ]
+        then
+                ((monthly_wage=$total_hours*$wage_per_hour))
+                echo Employee Attended $days_attended Days and worked $total_hours hours in them
+                echo Make monthly payment of $monthly_wage
+                echo Employee has reached limit of working this month
+                break;
+        elif [ $working_day -eq 21 ]
+        then
+                echo Employee Attended $days_attended Days and worked $total_hours hours in them
+                echo That is below minimum work threshhold for payment so cannot make payment right now
+        fi
+
+
+
 	#33% Chance that employee is absent
 	((attendance=RANDOM%3))
-	if [ $attendance -ne 0 ]
+	if [ $attendance -ne 0 -a $working_day -le 20 ]
 	then
 		((days_attended+=1))
 		echo Employee was present on Day $working_day
+	elif [ $working_day -eq 21 ]
+	then
+		continue
 	else
 		echo Employee was absent on Day $working_day
 		continue
@@ -27,35 +69,17 @@ do
 
 	if [ $employee_type -eq 0 ]
 	then
-		#Part time employee may work from 4 to 7 hours
-		((part_time_hour=$((RANDOM%4))+4))
-		todays_hours=$part_time_hour
-		((total_hours+=$part_time_hour))
-		echo Part time hours on Day $working_day : $part_time_hour
+		get_work_hours $employee_type
+		hours_today=$?
+		((total_hours+=$hours_today))
+		echo Part time hours on Day $working_day : $hours_today
 	else
-		#Full time employee may work from 6 to 9 hours
-		((full_time_hour=$((RANDOM%4))+6))
-		todays_hours=$full_time_hour
-		((total_hours+=$full_time_hour))
-                echo Full time hours on Day $working_day : $full_time_hour
+		get_work_hours $employee_type
+		hours_today=$?
+		((total_hours+=$hours_today))
+                echo Full time hours on Day $working_day : $hours_today
 	fi
 	((daily_wage=$todays_hours*$wage_per_hour))
 	echo Wage for Day $working_day is Rs. $daily_wage
-
-	 #Calculate wage for a maximum of 20 days or 100 hours
-        if [ $days_attended -eq 20 -o $total_hours -ge 100 ]
-        then
-                ((monthly_wage=$total_hours*$wage_per_hour))
-                echo Employee Attended $days_attended Days and worked $total_hours hours in them
-                echo Make monthly payment of $monthly_wage
-                echo Employee has reached limit of working this month
-                break;
-        elif [ $working_day -eq 20 ]
-        then
-                echo Employee Attended $days_attended Days and worked $total_hours hours in them
-                echo That is below minimum work threshhold for payment so cannot make payment right now
-        fi
-
-
 
 done
